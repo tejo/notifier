c73370895a2afaa913e50c919e0224906128d699
Matteo Parmi <m.parmi@gmail.com>
2022-05-14 10:02:27 +0200

    Add flags.

--- Makefile
+++ Makefile
@@ -4,5 +4,8 @@ test:
 build:
 	@(go build -o notify cmd/notify/main.go)
 
+run:
+	@(go run cmd/notify/main.go)
+
 server:
 	@(go build -o notify cmd/notify/main.go && ./notify -dummyserver) 
--- cmd/notify/main.go
+++ cmd/notify/main.go
@@ -1,9 +1,22 @@
 package main
 
 import (
+	"flag"
+	"fmt"
 	"notifier/pkg/notifier"
+	"time"
 )
 
 func main() {
+	var serverUrl string
+	var interval time.Duration
+	var runDummyServer bool
+
+	flag.StringVar(&serverUrl, "url", "", "URL of the server to post to")
+	flag.DurationVar(&interval, "interval", time.Second*5, "interval to wait between posts")
+	flag.BoolVar(&runDummyServer, "dummyserver", false, "runs a dummy server for testing")
+	flag.Parse()
+
+	fmt.Print(runDummyServer)
 	notifier.Notify("http://127.0.0.1:4567", "Hello, world!")
 }
